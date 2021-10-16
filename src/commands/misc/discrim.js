d4ce978c2e1450ea5cf5d4fa2c91041d4aa76188
skillzl <skillzlro@gmail.com>
2021-10-17 00:58:14 +0300

    misc commands

---
+++ src/commands/misc/discrim.js
@@ -0,0 +1,53 @@
+/** @format */
+
+const { embed } = require("../../utils/Utils");
+
+module.exports = class Discrim extends Command {
+	constructor() {
+		super({
+			name: "discrim",
+			aliases: ["discriminator"],
+			description:
+				"Searches for users with the specified discriminator. (or your own if not specified)",
+			usage: "<user>",
+			category: "Misc",
+			ownerOnly: false,
+			cooldown: 3000,
+			memberPerms: [],
+			clientPerms: [],
+		});
+	}
+	async exec(message, args) {
+		let discrim = args[0];
+		if (!discrim) {
+			discrim = message.author.discriminator;
+		}
+		if (discrim.startsWith("#")) {
+			discrim = discrim.slice(1);
+		}
+
+		if (/^[0-9]+$/.test(discrim) && discrim.length === 4) {
+			const users = this.client.users.cache
+				.filter((user) => user.discriminator === discrim)
+				.map((user) => user.username);
+
+			const splice = (s) =>
+				s.length > 1500 ? `${s.substring(0, 1490)}...` : s;
+
+			if (users.length === 0)
+				return message.reply(
+					`No user(s) with \`#${discrim}\` discriminator found.`
+				);
+
+			return message.reply(
+				`**${
+					users.length
+				}** user(s) found with the discriminator #${discrim}\n\`\`\`yml\n${splice(
+					users.join(", ").toString()
+				)}\`\`\``
+			);
+		} else {
+			return message.reply("Invalid discriminator provided.");
+		}
+	}
+};
---
+++ src/commands/misc/year.js
@@ -0,0 +1,29 @@
+/** @format */
+
+module.exports = class Year extends Command {
+	constructor() {
+		super({
+			name: "year",
+			aliases: ["year-progress"],
+			description: "Responds with the progress of the current year.",
+			usage: "",
+			category: "Misc",
+			ownerOnly: false,
+			cooldown: 3000,
+			memberPerms: [],
+			clientPerms: [],
+		});
+	}
+	async exec(message) {
+		const today = new Date();
+		const start = new Date(today.getFullYear(), 0, 1);
+		const end = new Date(today.getFullYear() + 1, 0, 1);
+		const percent = (Math.abs(today - start) / Math.abs(end - start)) * 100;
+
+		return message.reply(
+			`<:charliewave_calendar:826015486705401886> The year \`${today
+				.getFullYear()
+				.toFixed()}\` is \`${percent}%\` complete!`
+		);
+	}
+};
