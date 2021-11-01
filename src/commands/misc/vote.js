58ff051853d6efab90315535dad535219d641564
skillzl <user@skillzl.me>
2021-11-01 16:03:23 +0200

    cmd flags

--- src/commands/misc/vote.js
+++ src/commands/misc/vote.js
@@ -17,16 +17,21 @@ module.exports = class Vote extends Command {
 		});
 	}
 	async exec(message) {
-		const api = new Topgg.Api(
-			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc3MjQ5Nzc4OTU2MTIwODg3MiIsImJvdCI6dHJ1ZSwiaWF0IjoxNjEyMzc2ODYyfQ.5IuRwHOUflxD142D7jiHyGZFMEB1WNcyughGLrpNq9Q"
-		);
+    /* 
+		const api = new Topgg.Api("apiKey");
 
-		api.hasVoted(message.author.id).then((votes) => {
-			if (votes == false) {
-				message.reply(`It looks like you didn't vote today. You should check our bot on https://top.gg/bot/772497789561208872/vote`);
-			} else {
-				message.reply("You've already submitted a vote today. Try again later.");
-			}
-		});
-	}
+    api.hasVoted(message.author.id).then((votes) => {
+      if (votes == false) {
+        message.reply(
+          `It looks like you didn't vote today. You should check our bot on https://top.gg/bot/${this.client.user.id}}/vote`
+        );
+      } else {
+        message.reply(
+          "You've already submitted a vote today. Try again later."
+        );
+      }
+    });
+	*/
+    return message.reply("This command is disabled for a while.");
+  }
 };
