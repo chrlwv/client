9058d4cde2370dda66606f20da45877f55e65400
skillzl <user@skillzl.me>
2021-11-04 18:32:11 +0200

    ready event misc edits

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,28 +1,28 @@
 module.exports = class Ready extends Event {
-    constructor() {
-        super({
-            name: "ready",
-            once: false,
-        });
-    }
-    async exec() {
-        const servers = this.client.guilds.cache.size;
-        const users = this.client.guilds.cache.reduce(
-          (a, g) => a + g.memberCount,
-          0
-        );
-        const cachedUsers = this.client.users.cache.size;
+  constructor() {
+    super({
+      name: "ready",
+      once: false,
+    });
+  }
+  async exec() {
+    const servers = this.client.guilds.cache.size;
+    const users = this.client.guilds.cache.reduce(
+      (a, g) => a + g.memberCount,
+      0
+    );
+    const cachedUsers = this.client.users.cache.size;
 
-        this.client.logger.log(
-          `Interacted with ${servers.toLocaleString()} guilds and ${users.toLocaleString()} users (only ${cachedUsers.toLocaleString()} cached).`,
-          { tag: "Info" }
-        );
-        this.client.logger.log(`Client: ${this.client.user.tag} is online.`, {
-          tag: "Ready",
-        });
-        this.client.user.setActivity(`@${this.client.user.username} help`, {
-          type: "WATCHING",
-        });
-        await this.client.loadInteractions();
-    }
-}
+    this.client.logger.log(
+      `Interacted with ${servers.toLocaleString()} guilds and ${users.toLocaleString()} users (only ${cachedUsers.toLocaleString()} cached).`,
+      { tag: "Info" }
+    );
+    this.client.logger.log(`Client: ${this.client.user.tag} is online.`, {
+      tag: "Ready",
+    });
+    this.client.user.setActivity(`@${this.client.user.username} help`, {
+      type: "WATCHING",
+    });
+    await this.client.loadInteractions();
+  }
+};
