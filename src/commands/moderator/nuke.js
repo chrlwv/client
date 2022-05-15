d041cf00ed1c9b82c2d7d172015a2b67eb10bd16
skillzl <user@skillzl.me>
2022-05-15 18:32:39 +0300

    :globe_with_meridians: new: nuke command (moderator)

---
+++ src/commands/moderator/nuke.js
@@ -0,0 +1,24 @@
+/** @format */
+
+module.exports = class Nuke extends Command {
+  constructor() {
+    super({
+      name: "nuke",
+      aliases: ["nukechnl", "nukechannel"],
+      description: "Nukes the channel.",
+      usage: "",
+      category:
+        "<:charliewave_advanced_moderator:857930973715103775> Moderator",
+      ownerOnly: false,
+      cooldown: 3000,
+      memberPerms: ["MANAGE_ROLES"],
+      clientPerms: ["MANAGE_ROLES"],
+    });
+  }
+  async exec(message) {
+    await message.channel.clone().then((ch) =>{
+		ch.setParent(message.channel.parent.id);ch.setPosition(message.channel.position);
+        ch.send('This channel got nuked.');
+	}); message.channel.delete();
+  }
+};
