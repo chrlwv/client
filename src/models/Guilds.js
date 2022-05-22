d3c517686400798edda22b2800bb3099271b4ada
skillzl <user@skillzl.me>
2022-05-22 18:47:25 +0300

    Update Guilds.js

--- src/models/Guilds.js
+++ src/models/Guilds.js
@@ -6,7 +6,7 @@ module.exports = model(
   'Guilds',
   new Schema({
     guildId: { type: String },
-    prefix: { type: String, default: '/' },
+    prefix: { type: String, default: '-' },
     level_msg_module: { type: Boolean, default: true },
     uri_blocker_module: { type: Boolean, default: false },
     auto_role_module: { type: String, default: null },
