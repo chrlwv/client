21d3425c5e450c862f5e0395db4ea18067671ef3
skillzl <user@skillzl.me>
2022-06-10 15:47:09 +0300

    :zap: api: hypixel api catch err loop

--- src/commands/hypixel/skyblock.js
+++ src/commands/hypixel/skyblock.js
@@ -29,56 +29,57 @@ module.exports = class Skyblock extends Command {
       );
     let emb;
 
-    fetch(`https://api.minetools.eu/uuid/${args[0]}`)
-      .then((result) => result.json())
-      .then(async ({ id, name, status }) => {
-        const fetchProf = await fetch(
-          `https://api.slothpixel.me/api/skyblock/profile/${args[0]}`
-        );
-        const playerUUIDFetch = await fetch(
-          `https://api.minetools.eu/uuid/${args[0]}`
-        );
-        const playerUUIDData = await playerUUIDFetch.json();
-        const profileData = await fetchProf.json();
+    try {
+      fetch(`https://api.minetools.eu/uuid/${args[0]}`)
+        .then((result) => result.json())
+        .then(async ({ id, name, status }) => {
+          const fetchProf = await fetch(
+            `https://api.slothpixel.me/api/skyblock/profile/${args[0]}`
+          );
+          const playerUUIDFetch = await fetch(
+            `https://api.minetools.eu/uuid/${args[0]}`
+          );
+          const playerUUIDData = await playerUUIDFetch.json();
+          const profileData = await fetchProf.json();
 
-        if (status === "ERR") {
-          return message.reply("Could not fetch provided Mineacraft username.");
-        }
+          if (status === "ERR") {
+            return message.reply("Could not fetch provided Mineacraft username.");
+          }
 
-        const buttonWebsite = new MessageButton()
-          .setURL(`https://sky.shiiyu.moe/stats/${playerUUIDData.id}`)
-          .setLabel("Shiiyo.moe")
-          .setStyle("LINK");
+          const buttonWebsite = new MessageButton()
+            .setURL(`https://sky.shiiyu.moe/stats/${playerUUIDData.id}`)
+            .setLabel("Shiiyo.moe")
+            .setStyle("LINK");
 
-        const row = new MessageActionRow().addComponents(buttonWebsite);
+          const row = new MessageActionRow().addComponents(buttonWebsite);
 
-        emb = embed()
-          .setColor(0x36393e)
-          .setTitle(`${name}`)
-          .setURL(`https://sky.shiiyu.moe/stats/${playerUUIDData.id}`)
-          .setThumbnail(
-            `https://visage.surgeplay.com/head/512/${playerUUIDData.id}`
+          emb = embed()
+            .setColor(0x36393e)
+            .setTitle(`${name}`)
+            .setURL(`https://sky.shiiyu.moe/stats/${playerUUIDData.id}`)
+            .setThumbnail(
+              `https://visage.surgeplay.com/head/512/${playerUUIDData.id}`
+            )
+            .setDescription(profileData.cute_name);
+
+          if (
+            profileData.members[id].armor[3].name != null ||
+            (profileData.members[id].armor[3].name != undefined &&
+              profileData.members[id].armor[2].name != null) ||
+            (profileData.members[id].armor[2].name != undefined &&
+              profileData.members[id].armor[2].name != null) ||
+            (profileData.members[id].armor[2].name != undefined &&
+              profileData.members[id].armor[1].name != null) ||
+            profileData.members[id].armor[1].name != undefined
           )
-          .setDescription(profileData.cute_name);
-
-        if (
-          profileData.members[id].armor[3].name != null ||
-          (profileData.members[id].armor[3].name != undefined &&
-            profileData.members[id].armor[2].name != null) ||
-          (profileData.members[id].armor[2].name != undefined &&
-            profileData.members[id].armor[2].name != null) ||
-          (profileData.members[id].armor[2].name != undefined &&
-            profileData.members[id].armor[1].name != null) ||
-          profileData.members[id].armor[1].name != undefined
-        )
-          emb.addField(
-            "**ARMOR:**",
-            profileData.members[id].armor[3].name
-              .replace(/§/g, "")
-              .replace(/\d+/, "")
-              .replace("d", "")
-              .replace("f", "")
-              .replace(/6/g, "") +
+            emb.addField(
+              "**ARMOR:**",
+              profileData.members[id].armor[3].name
+                .replace(/§/g, "")
+                .replace(/\d+/, "")
+                .replace("d", "")
+                .replace("f", "")
+                .replace(/6/g, "") +
               `\n` +
               profileData.members[id].armor[2].name
                 .replace(/§/g, "")
@@ -100,100 +101,101 @@ module.exports = class Skyblock extends Command {
                 .replace("d", "")
                 .replace("f", "")
                 .replace(/6/g, "")
+            );
+          emb.addField(
+            "**STATS:**",
+            `<:Health_icon:852570156261834762> ${profileData.members[
+              id
+            ].attributes.health.toLocaleString()} | <:Absorption_icon:852570156110184489> ${profileData.members[
+              id
+            ].attributes.effective_health.toLocaleString()} \n<:Defense_icon:852570156211503109> ${profileData.members[
+              id
+            ].attributes.defense.toLocaleString()} | <:Strength_icon:852570155966791692> ${profileData.members[
+              id
+            ].attributes.strength.toLocaleString()}\n<:Speed_icon:852570156198133790> ${profileData.members[
+              id
+            ].attributes.speed.toLocaleString()}% | <:Intelligence_icon:852570156239683594> ${profileData.members[
+              id
+            ].attributes.intelligence.toLocaleString()}`,
+            true
           );
-        emb.addField(
-          "**STATS:**",
-          `<:Health_icon:852570156261834762> ${profileData.members[
-            id
-          ].attributes.health.toLocaleString()} | <:Absorption_icon:852570156110184489> ${profileData.members[
-            id
-          ].attributes.effective_health.toLocaleString()} \n<:Defense_icon:852570156211503109> ${profileData.members[
-            id
-          ].attributes.defense.toLocaleString()} | <:Strength_icon:852570155966791692> ${profileData.members[
-            id
-          ].attributes.strength.toLocaleString()}\n<:Speed_icon:852570156198133790> ${profileData.members[
-            id
-          ].attributes.speed.toLocaleString()}% | <:Intelligence_icon:852570156239683594> ${profileData.members[
-            id
-          ].attributes.intelligence.toLocaleString()}`,
-          true
-        );
 
-        let petStatsContainter;
+          let petStatsContainter;
 
-        const petStatistics = `[LVL ${
-          profileData.members[id].active_pet.level
-        }] ${profileData.members[id].active_pet.name}\n${
-          profileData.members[id].active_pet.rarity
-        }\nExp: ${numAbbr.abbreviate(profileData.members[id].active_pet.exp)}`;
+          const petStatistics = `[LVL ${profileData.members[id].active_pet.level
+            }] ${profileData.members[id].active_pet.name}\n${profileData.members[id].active_pet.rarity
+            }\nExp: ${numAbbr.abbreviate(profileData.members[id].active_pet.exp)}`;
 
-        if (petStatistics != null || petStatistics != undefined)
-          petStatsContainter = petStatistics;
+          if (petStatistics != null || petStatistics != undefined)
+            petStatsContainter = petStatistics;
 
-        if (petStatsContainter.includes("undefined"))
-          petStatsContainter = "No Active Pet";
+          if (petStatsContainter.includes("undefined"))
+            petStatsContainter = "No Active Pet";
 
-        emb.addField("**ACTIVE PET:**", petStatsContainter, true);
-        emb.addField(
-          "**AUCTIONS:**",
-          `Fees: ${numAbbr.abbreviate(
-            profileData.members[id].stats.auctions.total_fees
-          )}\nGold Earned: ${numAbbr.abbreviate(
-            profileData.members[id].stats.auctions.gold_earned
-          )}\nGold Spent: ${numAbbr.abbreviate(
-            profileData.members[id].stats.auctions.gold_spent
-          )}`,
-          true
-        );
-        emb.addField(
-          "**AVG SKILL LEVEL:**",
-          `${profileData.members[id].average_skill_level}`,
-          true
-        );
-        emb.addField(
-          "**FAIRY SOULS:**",
-          `${profileData.members[id].fairy_souls_collected}/238`,
-          true
-        );
-        emb.addField(
-          "**HIGHEST DELT DMG:**",
-          `${numAbbr.abbreviate(
-            profileData.members[id].stats.highest_critical_damage
-          )}`,
-          true
-        );
-        emb.addField(
-          "**GIFTS:**",
-          `Given: ${numAbbr.abbreviate(
-            profileData.members[id].stats.gifts_given
-          )}\nReceived: ${numAbbr.abbreviate(
-            profileData.members[id].stats.gifts_received
-          )}`,
-          true
-        );
+          emb.addField("**ACTIVE PET:**", petStatsContainter, true);
+          emb.addField(
+            "**AUCTIONS:**",
+            `Fees: ${numAbbr.abbreviate(
+              profileData.members[id].stats.auctions.total_fees
+            )}\nGold Earned: ${numAbbr.abbreviate(
+              profileData.members[id].stats.auctions.gold_earned
+            )}\nGold Spent: ${numAbbr.abbreviate(
+              profileData.members[id].stats.auctions.gold_spent
+            )}`,
+            true
+          );
+          emb.addField(
+            "**AVG SKILL LEVEL:**",
+            `${profileData.members[id].average_skill_level}`,
+            true
+          );
+          emb.addField(
+            "**FAIRY SOULS:**",
+            `${profileData.members[id].fairy_souls_collected}/238`,
+            true
+          );
+          emb.addField(
+            "**HIGHEST DELT DMG:**",
+            `${numAbbr.abbreviate(
+              profileData.members[id].stats.highest_critical_damage
+            )}`,
+            true
+          );
+          emb.addField(
+            "**GIFTS:**",
+            `Given: ${numAbbr.abbreviate(
+              profileData.members[id].stats.gifts_given
+            )}\nReceived: ${numAbbr.abbreviate(
+              profileData.members[id].stats.gifts_received
+            )}`,
+            true
+          );
 
-        let BankStatsContainter;
+          let BankStatsContainter;
 
-        const BankStatistics = `Bank: ${numAbbr.abbreviate(
-          profileData.banking.balance
-        )}\nPurse: ${numAbbr.abbreviate(profileData.members[id].coin_purse)}`;
+          const BankStatistics = `Bank: ${numAbbr.abbreviate(
+            profileData.banking.balance
+          )}\nPurse: ${numAbbr.abbreviate(profileData.members[id].coin_purse)}`;
 
-        if (BankStatistics != null || BankStatistics != undefined)
-          BankStatsContainter = BankStatistics;
+          if (BankStatistics != null || BankStatistics != undefined)
+            BankStatsContainter = BankStatistics;
 
-        if (BankStatsContainter.includes("null")) BankStatsContainter = "0";
+          if (BankStatsContainter.includes("null")) BankStatsContainter = "0";
 
-        emb.addField("**COINS:**", BankStatsContainter, true);
-        emb.addField(
-          "**JERRY'S WORKSHOP:**",
-          `Snowballs Hits: ${numAbbr.abbreviate(
-            profileData.members[id].stats.winter_records.snowballs_hit
-          )}\nDamage: ${numAbbr.abbreviate(
-            profileData.members[id].stats.winter_records.damage
-          )}`,
-          true
-        );
-        return message.reply({ embeds: [emb], components: [row] });
-      });
+          emb.addField("**COINS:**", BankStatsContainter, true);
+          emb.addField(
+            "**JERRY'S WORKSHOP:**",
+            `Snowballs Hits: ${numAbbr.abbreviate(
+              profileData.members[id].stats.winter_records.snowballs_hit
+            )}\nDamage: ${numAbbr.abbreviate(
+              profileData.members[id].stats.winter_records.damage
+            )}`,
+            true
+          );
+          return message.reply({ embeds: [emb], components: [row] });
+        });
+    } catch (err) {
+      this.logger.error(`Error: ${err.message}`, { tag: 'SkyblockAPIError' })
+    }
   }
 };
